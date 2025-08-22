1d1619ff6062192ce47fa33e2d2ff042ce26b065
CrashTestAlex <alexkola109@icloud.com>
2025-08-22 19:10:30 -0400

    i dont wanna talk abt it

--- CBattle/package/logic.py
+++ CBattle/package/logic.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Type
 
@@ -7,12 +8,34 @@ from ballsdex.core.models import BallInstance, Player
 
 from .base import BaseEffect
 
-# from .config import max_deck_size
-
 if TYPE_CHECKING:
-    from discord import Channel, Member, User
+    from discord import Member, User
 
-    from .components import BattleAcceptView, TurnView
+    from .components import BattleAcceptView
+
+ATTACK_MESSAGES = [
+    "{a_owner}'s {a_name} attacks {d_owner}'s {d_name} for {dmg} DMG!",
+    "{a_name} uppercuts {d_name}, dealing {dmg} DMG!",
+    "{a_owner}'s {a_name} slices {d_name}! ({dmg} DMG)",
+    "{a_name} lands a solid blow on {d_name} for {dmg} DMG",
+]
+
+DEFEAT_MESSAGES = [
+    "{a_name} has easily crushed {d_name}!",
+    "{d_owner}'s {d_name} has fallen to {a_owner}'s {a_name}.",
+    "{a_name} knocks out {d_name}!",
+    "{d_name} has been defeated!",
+]
+
+DODGE_MESSAGES = [
+    "{a_name} tries to land a blow, but {d_name} dodges!",
+    "{d_owner}'s {d_name} evades {a_owner}'s {a_name} attack!",
+    "{d_name} sidesteps the attack!",
+]
+
+
+def format_random(msg_list, **kwargs):
+    return random.choice(msg_list).format(**kwargs)
 
 
 @dataclass
@@ -23,6 +46,7 @@ class BattleBall:
 
     model: BallInstance
     health: int
+    dead: bool = False
 
     effects: list[BaseEffect] = field(default_factory=list)
 
@@ -30,13 +54,9 @@ class BattleBall:
     def from_ballinstance(cls, ballinstance: BallInstance):
         return cls(model=ballinstance, health=ballinstance.health)
 
-    # @property
-    # def damage(self) -> int:
-    #     base = model.attack * random.uniform(0.5, 1)
-    #     is_super = random.random() < 0.25
-    #     if is_super:
-    #         return int(base * 1.5), True
-    #     return int(base), False
+    @property
+    def damage(self):
+        return int(self.model.attack * random.uniform(0.6, 1.2))
 
     def apply_effect(self, effect: Type[BaseEffect], rounds: int):
         self.effects.append(effect(self, rounds))
@@ -51,7 +71,6 @@ class BattlePlayer:
     model: Player
     user: User | Member
     balls: list[BattleBall] = field(default_factory=list)
-    locked: bool = False
 
 
 @dataclass
@@ -62,20 +81,99 @@ class BattleState:
 
     player1: BattlePlayer
     player2: BattlePlayer
-    channel: Channel
+    winner: User | Member
 
     turns: int = 0
     started: bool = False
     accepted: bool = False
-    winner: str = ""
     accept_view: BattleAcceptView | None = None
-    last_turn: TurnView | None = None
-    # max_deck_size: int = max_deck_size
+   # max_deck_size: int = max_deck_size
 
-    def get_user(self, user: User | Member) -> BattlePlayer | None:
-        if self.player1.user == user:
-            return self.player1
-        if self.player2.user == user:
-            return self.player2
 
-        return None
+def attack(current_ball, opponent_balls):
+    alive_balls = [ball for ball in opponent_balls if not ball.dead]
+    opponent = random.choice(alive_balls)
+
+    damage = current_ball.damage
+    opponent.health -= damage
+    if opponent.health <= 0:
+        opponent.health = 0
+        opponent.dead = True
+
+    if opponent.dead:
+        text = format_random(
+            DEFEAT_MESSAGES,
+            a_owner=current_ball.owner,
+            a_name=current_ball.name,
+            d_owner=opponent.owner,
+            d_name=opponent.name,
+            dmg=damage,
+        )
+    else:
+        text = format_random(
+            ATTACK_MESSAGES,
+            a_owner=current_ball.owner,
+            a_name=current_ball.name,
+            d_owner=opponent.owner,
+            d_name=opponent.name,
+            dmg=damage,
+        )
+
+    return text
+
+
+def random_events(p1_ball, p2_ball):
+    if random.randint(1, 100) <= 25:
+        msg = format_random(
+            DODGE_MESSAGES,
+            a_owner=p2_ball.owner,
+            a_name=p2_ball.name,
+            d_owner=p1_ball.owner,
+            d_name=p1_ball.name,
+        )
+        return True, msg
+    return False, ""
+
+
+def gen_battle(battle: BattleState):
+    turn = 0
+
+    while any(ball for ball in battle.player1.balls if not ball.dead) and any(
+        ball for ball in battle.player2.balls if not ball.dead
+    ):
+        alive_p1 = [ball for ball in battle.player1.balls if not ball.dead]
+        alive_p2 = [ball for ball in battle.player2.balls if not ball.dead]
+
+        for p1_ball, p2_ball in zip(alive_p1, alive_p2):
+            if not p1_ball.dead:
+                turn += 1
+
+                event = random_events(p1_ball, p2_ball)
+                if event[0]:
+                    yield f"Turn {turn}: {event[1]}"
+                    continue
+
+                yield f"Turn {turn}: {attack(p1_ball, battle.player2.balls)}"
+
+                if all(ball.dead for ball in battle.player2.balls):
+                    break
+
+            if not p2_ball.dead:
+                turn += 1
+
+                event = random_events(p2_ball, p1_ball)
+                if event[0]:
+                    yield f"Turn {turn}: {event[1]}"
+                    continue
+
+                yield f"Turn {turn}: {attack(p2_ball, battle.player1.balls)}"
+
+                if all(ball.dead for ball in battle.player1.balls):
+                    break
+
+    if all(ball.dead for ball in battle.player1.balls):
+        battle.winner = battle.player2.user
+    elif all(ball.dead for ball in battle.player2.balls):
+        battle.winner = battle.player1.user
+
+    battle.turns = turn
