1ae887043a3ac18c09fbbfe6a390b09d4c72242a
dormieriancitizen <dormieriancitizen@gmail.com>
2025-09-13 12:58:26 -0400

    Allow user to choose remote in cb.cbattle command

--- CBattle/package/commands.py
+++ CBattle/package/commands.py
@@ -16,7 +16,7 @@ class CBattleText(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def cbattle(self, ctx: commands.Context, reference: str = "main"):
+    async def cbattle(self, ctx: commands.Context, reference: str = "main", remote: str = "Dotsian/CBattle"):
         """
         Displays the CBattle installer.
 
@@ -25,13 +25,13 @@ class CBattleText(commands.Cog):
         reference: str
             The CBattle branch you want to run the installer on.
         """
-        link = "https://api.github.com/repos/Dotsian/CBattle/contents/CBattle/github/installer.py"
+        link = f"https://api.github.com/repos/{remote}/contents/CBattle/github/installer.py"
 
         request = requests.get(link, {"ref": reference})
 
         match request.status_code:
             case requests.codes.not_found:
-                await ctx.send(f"Could not find installer for the {reference} branch.")
+                await ctx.send(f"Could not find installer for the {reference} branch on {remote}.")
 
             case requests.codes.ok:
                 content = requests.get(link, {"ref": reference}).json()["content"]
